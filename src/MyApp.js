da3da2512024d79565b2a08e89a34081188eb94c
thaliaward <thalia.ward@yahoo.com>
2021-01-08 16:11:41 -0800

    Added Delete buttons

--- src/MyApp.js
+++ src/MyApp.js
@@ -1,31 +1,38 @@
-import React from 'react'
+import React, {useState} from 'react'
 import Table from './Table'
 
-const characters = [
-    {
-        name: 'Charlie',
-        job: 'Janitor',
-    },
-    {
-        name: 'Mac',
-        job: 'Bouncer',
-    },
-    {
-        name: 'Dee',
-        job: 'Aspiring Actress',
-    },
-    {
-        name: 'Dennis',
-        job: 'Bartender',
-    },
-];
-
 function MyApp() {
-    return ( 
-      <div className="container"> 
-         <Table characterData={characters} />
-      </div> 
-    ); 
+    const [characters, setCharacters] = useState([
+        {
+            name: 'Charlie',
+            job: 'Janitor',
+        },
+        {
+            name: 'Mac',
+            job: 'Bouncer',
+        },
+        {
+            name: 'Dee',
+            job: 'Aspiring Actress',
+        },
+        {
+            name: 'Dennis',
+            job: 'Bartender',
+        },
+    ]);
+
+    function removeOneCharacter (index) {
+        const updated = characters.filter((character, i) => {
+            return i !== index
+        });
+        setCharacters(updated);
+    }
+
+    return (
+        <div className="container">
+            <Table characterData={characters} removeCharacter={removeOneCharacter} />
+        </div>
+    )
 }
 
 export default MyApp;
